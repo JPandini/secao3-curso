b2c93936c2c560203283a174676b7a3bd79ff2f2
Pandini <pandinijoao@gmail.com>
2023-05-16 12:48:33 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+//----------AULA 1----------
+// setInterval
+
+//function acao() {
+//    document.write('Escrevendo... <br/>')
+//}
+//setInterval(acao, 1000) //dois parametros (função e tempo em ms)
+//------------------
+
+/* setInterval (()=>{ //função anonima (arrow =>)
+    document.write('Escrevendo... <br/>');
+},1000) */
+
+//setTimeout
+
+function acao() {
+    document.write('Escrevendo... <br/>')
+}
+
+
+setTimeout(acao, 3000) //dois parametros (função e tempo em ms) // executa somente uma vez
